5089d4551fdf317a8bea02b4e3133c50edacf30e
AnnaZver <91017938+AnnaZver@users.noreply.github.com>
2021-10-03 12:40:45 +0300

    Update 07_task_tuple.py

--- Module3/practice/list_and_tuple/07_task_tuple.py
+++ Module3/practice/list_and_tuple/07_task_tuple.py
@@ -3,3 +3,10 @@
 tup = (2, 4, 6, -4, 12, 0, 5)
 
 # TODO: your code here
+tup = (2, 4, 6, -4, 12, 0, 5)
+long_num = tup[0]
+for num in tup:
+    if num > long_num:
+        long_num = num
+
+print(long_num)
