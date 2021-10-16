fb7305ae9b2f9e47f1c0e9de38646360d1bf0a53
AnnaZver <91017938+AnnaZver@users.noreply.github.com>
2021-10-16 20:15:17 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -1,5 +1,9 @@
 # Дан файл data/info.txt, в каждой строке которого содержится строка или целое число
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
-with open("data/info.txt", "r") as f:
-    pass
+sum = 0
+with open("log1.txt", "r") as f:
+    for line in f:
+        if line.strip().isdigit():
+            sum += int(line)
+print(sum)
